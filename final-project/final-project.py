692d2b2c80f71d4eeb8acd3906eca4fa20682178
dskrenta <dskrenta@gmail.com>
2018-12-08 00:09:25 -0700

    problem 4 work

--- final-project/final-project.py
+++ final-project/final-project.py
@@ -1,3 +1,5 @@
+from random import uniform
+
 def pandasPeril():
   cards = [5, 10, 15, 5, 10, 20, 4, 1]
   dynamicExpectedSum = 0
@@ -14,12 +16,15 @@ def pandasPeril():
   def dynamic(cards):
     left = cards[0]
     right = cards[len(cards) - 1]
-    expectedSum = 0
+    leftNext = 0
+    rightNext = 0
 
     if left > right: 
       # player 2 will pick left
       if (right + leftNext) > (left + rightNext):
-        None
+        cards.pop()
+      else: 
+        cards.pop(0)
       None
     elif left < right: 
       # player 2 will pick right
@@ -74,4 +79,77 @@ def problem1():
 
   # B and C are human (Homo Sapiens), A is soybean (Glycine Max) because b and c have the longest common substring
 
-problem1()
+# problem1()
+
+def convex_hull(points):
+  """Computes the convex hull of a set of 2D points.
+
+  Input: an iterable sequence of (x, y) pairs representing the points.
+  Output: a list of vertices of the convex hull in counter-clockwise order,
+    starting from the vertex with the lexicographically smallest coordinates.
+  Implements Andrew's monotone chain algorithm. O(n log n) complexity.
+  """
+
+  # Sort the points lexicographically (tuples are compared lexicographically).
+  # Remove duplicates to detect the case we have just one unique point.
+  points = sorted(set(points))
+
+  # Boring case: no points or a single point, possibly repeated multiple times.
+  if len(points) <= 1:
+    return points
+
+  # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+  # Returns a positive value, if OAB makes a counter-clockwise turn,
+  # negative for clockwise turn, and zero if the points are collinear.
+  def cross(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+  # Build lower hull 
+  lower = []
+  for p in points:
+    while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+        lower.pop()
+    lower.append(p)
+
+  # Build upper hull
+  upper = []
+  for p in reversed(points):
+    while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+      upper.pop()
+    upper.append(p)
+
+  # Concatenation of the lower and upper hulls gives the convex hull.
+  # Last point of each list is omitted because it is repeated at the beginning of the other list. 
+  return lower[:-1] + upper[:-1]
+
+def randCoords(n):
+  def newPoint():
+    return (uniform(0, 100), uniform(0, 100))
+
+  points = []
+
+  for x in range(1, n):
+    points.append(newPoint())
+
+  return points
+
+def problem4():
+  points = randCoords(50)
+  convex_hull_points = convex_hull(points)
+
+  max_x = max(convex_hull_points, key=lambda x: x[0])[0]
+  max_y = max(convex_hull_points, key=lambda x: x[1])[1]
+  min_x = min(convex_hull_points, key=lambda x: x[0])[0]
+  min_y = min(convex_hull_points, key=lambda x: x[1])[1]
+
+  r_coords = [
+    (min_x, max_y),
+    (min_x, min_y),
+    (max_x, max_y),
+    (max_x, min_y)
+  ]
+
+  print(points)
+  print(r_coords)
+
+problem4()
