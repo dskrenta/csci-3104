4ef970717c56b9b77b124551a8f40e024cc9f893
dskrenta <dskrenta@gmail.com>
2018-12-14 00:35:02 -0700

    additional progress

--- assignment11/assignment11.py
+++ assignment11/assignment11.py
@@ -1,6 +1,4 @@
-from random import randint
-
-BUCKETS_COUNT = 100
+import random
 
 def indexOfLetter(x):
   letters = [
@@ -13,13 +11,43 @@ def indexOfLetter(x):
   ]
   return letters.index(x) + 1
 
-def h1(x):
+def h1(x, l):
   xList = list(x)
-  return sum(map(lambda x: indexOfLetter(x), xList)) % BUCKETS_COUNT
+  return sum(map(lambda x: indexOfLetter(x), xList)) % l
 
-def h2(x):
-  rand = randint(0, BUCKETS_COUNT - 1)
+def h2(x, l):
+  rand = random.randint(0, l - 1)
   xList = list(x)
-  return sum(map(lambda x: indexOfLetter(x) * rand, xList)) % BUCKETS_COUNT
+  return sum(map(lambda x: indexOfLetter(x) * rand, xList)) % l
 
-print(h1('stuff'), h2('stuff'))
+def a(names):
+  h1Dict = {}
+  h2Dict = {}
+
+  def addToDict(data, value):
+    if value in data:
+      data[value] += 1
+    else:
+      data[value] = 1
+
+  for name in names:
+    addToDict(h1Dict, h1(name, 5701))
+    addToDict(h2Dict, h2(name, 5701))
+  
+  return h1Dict, h2Dict
+
+def b(data):
+  return False
+
+def main():
+  file = open('dist.all.last.txt', 'r')
+  names = list(map(lambda x: x.split('\t')[0].lower(), file.readlines()))
+  random.shuffle(names)
+  selectedNames = names[:len(names) // 2]
+  # print(selectedNames, len(names), len(selectedNames))
+  result = a(selectedNames)
+  print(result)
+
+main()
+
+# print(h1('stuff'), h2('stuff'))
